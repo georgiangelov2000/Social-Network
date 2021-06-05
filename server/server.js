87c651ac750eec8b34efa1fd199ff650cf124143
SoftAcademy/Plovdiv University/Georgi Angelov <60781748+georgiangelov2000@users.noreply.github.com>
2021-06-05 16:42:35 +0300

    added cors

--- server/server.js
+++ server/server.js
@@ -1,21 +1,24 @@
 const express = require("express");
 const connectDB = require("./config/db");
+const cors = require("cors");
+
 const app = express();
 
 //Connect to Database
 connectDB();
+app.use(cors());
 
 app.use(express.json({ extended: false }));
 
-const usersRoute= require("./routes/users");
-const authRoute=require("./routes/auth");
-const profileRoute=require("./routes/profile");
-const postsRoute=require("./routes/posts");
+const usersRoute = require("./routes/users");
+const authRoute = require("./routes/auth");
+const profileRoute = require("./routes/profile");
+const postsRoute = require("./routes/posts");
 
-app.use("/api/users",usersRoute);
-app.use("/api/auth",authRoute);
-app.use("/api/profile",profileRoute);
-app.use("/api/posts",postsRoute);
+app.use("/api/users", usersRoute);
+app.use("/api/auth", authRoute);
+app.use("/api/profile", profileRoute);
+app.use("/api/posts", postsRoute);
 
 const PORT = process.env.PORT || 5000;
 
